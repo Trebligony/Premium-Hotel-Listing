41046e3e96e3592c0647f523198fdd721ebec2e4
Keith S Thomas <kthomasnj@hotmail.com>
2022-08-04 19:32:08 -0400

    Updating script2.js

--- js/script2.js
+++ js/script2.js
@@ -1,28 +1,33 @@
 var map;
 var service;
 var infowindow;
+var submitBtn = document.getElementById('submit');
 
 function initMap() {
-console.log("I'm running initMap Function");
+  console.log("I'm running initMap Function");
+  console.log(document.getElementById('search-value').value);
   var sydney = new google.maps.LatLng(-33.867, 151.195);
 
   infowindow = new google.maps.InfoWindow();
 
   map = new google.maps.Map(
-      document.getElementById('map'), {center: sydney, zoom: 15});
+    document.getElementById('map'), { center: sydney, zoom: 15 });
 
+  // map = new google.maps.Map();
+
+  
   var request = {
-    query: 'IHOP',
+    query: searchValue,
     fields: ['name', 'icon', 'geometry', 'price_level', 'rating'],
   };
 
   var service = new google.maps.places.PlacesService(map);
 
-  service.findPlaceFromQuery(request, function(results, status) {
+  service.findPlaceFromQuery(request, function (results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
       for (var i = 0; i < results.length; i++) {
         console.log(results);
-        createMarker(results[i]);
+        // createMarker(results[i]);
       }
       map.setCenter(results[0].geometry.location);
     }
@@ -31,17 +36,19 @@ console.log("I'm running initMap Function");
 
 
 function createMarker(place) {
-    if (!place.geometry || !place.geometry.location) return;
-  
-    const marker = new google.maps.Marker({
-      map,
-      position: place.geometry.location,
-    });
-  
-    google.maps.event.addListener(marker, "click", () => {
-      infowindow.setContent(place.name || "");
-      infowindow.open(map);
-    });
-  }
+  if (!place.geometry || !place.geometry.location) return;
 
-// initMap();
+  const marker = new google.maps.Marker({
+    map,
+    position: place.geometry.location,
+  });
+
+  google.maps.event.addListener(marker, "click", () => {
+    infowindow.setContent(place.name || "");
+    infowindow.open(map);
+  });
+}
+
+submitBtn.addEventListener("click", function () {
+  initMap();
+});
