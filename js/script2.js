227ade2c0e3c075bfd67e849c6fc980fe8754503
Keith S Thomas <kthomasnj@hotmail.com>
2022-08-07 20:01:20 -0400

    Added search functionality of adding search results to index.html by clicking the search button.

--- js/script2.js
+++ js/script2.js
@@ -1,17 +1,16 @@
-<<<<<<< HEAD
 var keyword = document.querySelector('#search-text');
 var submitBtn = document.querySelector('#submit');
-var googleMaps = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?&location=-40.719074%2C-74.050552&radius=1500&type=restaurant';
+var googleMaps = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?&location=-33.8670522%2C151.1957362&radius=1500&type=restaurant';
 var searchResults = document.querySelector('.restaurants_list');
 
 
-submitBtn.addEventListener('submit', function () {
-  event.preventDefault();
-  console.log('button works!!!');
+submitBtn.addEventListener('click', function () {
     var keyWord = keyword.value;
     var requestUrl = googleMaps + "&keyword=" + keyWord + "&key=" + apiKey;
-    document.querySelector('.restaurants_list').getElementsByClassName.display = block;
-    
+    var searchResultsEl = document.querySelector('.restaurants_list');
+
+    console.log(requestUrl);
+ 
     fetch(requestUrl)    
     .then(function (response) {        
         return response.json();
@@ -31,9 +30,9 @@ submitBtn.addEventListener('submit', function () {
             businessNameEl.textContent = businessName;
             businessDetailsEl.textContent = businessDetails;
 
-            searchResults.append(businessNameEl);
-            searchResults.append(businessDetailsEl);
-            searchResults.append(horizontalLineEl);
+            searchResultsEl.append(businessNameEl);
+            searchResultsEl.append(businessDetailsEl);
+            searchResultsEl.append(horizontalLineEl);
           }        
     });
 })
@@ -92,60 +91,60 @@ submitBtn.addEventListener('submit', function () {
 //   event.preventDefault();
 //   initMap();
 // });
-=======
-var map;
-var service;
-var infowindow;
-var submitBtn = document.getElementById('submit');
-var searchVal = document.getElementById('search-text');
+// =======
+// var map;
+// var service;
+// var infowindow;
+// var submitBtn = document.getElementById('submit');
+// var searchVal = document.getElementById('search-text');
 
-function initMap() {
-  console.log(searchVal.value);
-  var sydney = new google.maps.LatLng(-33.867, 151.195);
+// function initMap() {
+//   console.log(searchVal.value);
+//   var sydney = new google.maps.LatLng(-33.867, 151.195);
 
-  infowindow = new google.maps.InfoWindow();
+//   infowindow = new google.maps.InfoWindow();
 
-  map = new google.maps.Map(
-    document.getElementById('map'), { center: sydney, zoom: 15 });
+//   map = new google.maps.Map(
+//     document.getElementById('map'), { center: sydney, zoom: 15 });
 
-  // map = new google.maps.Map();
+//   // map = new google.maps.Map();
   
-  var request = {
-    query: searchVal,
-    fields: ['name', 'icon', 'geometry', 'price_level', 'rating'],
-  };
+//   var request = {
+//     query: searchVal,
+//     fields: ['name', 'icon', 'geometry', 'price_level', 'rating'],
+//   };
 
-  var service = new google.maps.places.PlacesService(map);
+//   var service = new google.maps.places.PlacesService(map);
 
-  service.findPlaceFromQuery(request, function (results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        console.log(results);
-        createMarker(results[i]);
-      }
-      map.setCenter(results[0].geometry.location);
-    }
-  });
-}
+//   service.findPlaceFromQuery(request, function (results, status) {
+//     if (status === google.maps.places.PlacesServiceStatus.OK) {
+//       for (var i = 0; i < results.length; i++) {
+//         console.log(results);
+//         createMarker(results[i]);
+//       }
+//       map.setCenter(results[0].geometry.location);
+//     }
+//   });
+// }
 
 
-function createMarker(place) {
-  if (!place.geometry || !place.geometry.location) return;
+// function createMarker(place) {
+//   if (!place.geometry || !place.geometry.location) return;
 
-  const marker = new google.maps.Marker({
-    map,
-    position: place.geometry.location,
-  });
+//   const marker = new google.maps.Marker({
+//     map,
+//     position: place.geometry.location,
+//   });
 
-  google.maps.event.addListener(marker, "click", () => {
-    infowindow.setContent(place.name || "");
-    infowindow.open(map);
-  });
-}
+//   google.maps.event.addListener(marker, "click", () => {
+//     infowindow.setContent(place.name || "");
+//     infowindow.open(map);
+//   });
+// }
 
-submitBtn.addEventListener("click", function() {
-  event.preventDefault();
-  initMap();
-});
->>>>>>> 80c3caeff31c02ccf4f36b9a1d441ea6bde77063
+// submitBtn.addEventListener("click", function() {
+//   event.preventDefault();
+//   initMap();
+// });
+// >>>>>>> 80c3caeff31c02ccf4f36b9a1d441ea6bde77063
 
