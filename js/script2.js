7403b0ae8300e934cfdd6ca00ced9d5e33718a38
JGarcia128 <jeidigarcia123@gmail.com>
2022-08-08 22:41:02 -0400

    Update script2.js

--- js/script2.js
+++ js/script2.js
@@ -40,6 +40,108 @@ submitBtn.addEventListener('click', function () {
     });
 })
 
+var rootEl = $('.list_title')
+//var timeEl = $('#currentDay');
+var saveBtn = $('.savebtn');
+//var currentTime = moment().format("dddd, MMMM Do");
+//var currentTimeHr = moment().format("HH");
+//var curentTimeHrInt = moment().format("HH");
+//var hours = [moment("1-1-2027 08:00:00").format("ha"),moment("1-1-2027 09:00:00").format("ha"), moment("1-1-2027 10:00:00").format("ha"), moment("1-1-2027 11:00:00").format("ha"), moment("1-1-2027 12:00:00").format("ha"), moment("1-1-2027 13:00:00").format("ha"), moment("1-1-2027 14:00:00").format("ha"), moment("1-1-2027 15:00:00").format("ha"), moment("1-1-2027 16:00:00").format("ha"), moment("1-1-2027 17:00:00").format("ha"), moment("1-1-2027 18:00:00").format("ha")];
+//var hoursInts = [moment("1-1-2027 08:00:00").format("HH"), moment("1-1-2027 09:00:00").format("HH"), moment("1-1-2027 10:00:00").format("H"), moment("1-1-2027 10:00:00").format("HH"), moment("1-1-2027 11:00:00").format("HH"), moment("1-1-2027 12:00:00").format("HH"), moment("1-1-2027 13:00:00").format("HH"), moment("1-1-2027 14:00:00").format("HH"), moment("1-1-2027 15:00:00").format("HH"), moment("1-1-2027 16:00:00").format("HH"), moment("1-1-2027 17:00:00").format("HH"), moment("1-1-2027 18:00:00").format("HH")];
+function writeFromLocalStorage () {
+    for (let i = 0; i < hours.length; i++) {
+        var mr1 = localStorage.getItem('mrs1');
+        var mr2 = localStorage.getItem('mrs2');
+        var mr3 = localStorage.getItem('mrs3');
+        var mr4 = localStorage.getItem('mrs4');
+        var mr5 = localStorage.getItem('mrs5');
+        var mr6 = localStorage.getItem('mrs6');
+        var mr7 = localStorage.getItem('mrs7');
+        var mr8 = localStorage.getItem('mrs8');
+        var mr9 = localStorage.getItem('mrs9');
+        var mr10 = localStorage.getItem('mrs10');
+        var mr11 = localStorage.getItem('mrs11');
+        var mr12 = localStorage.getItem('mrs12');
+        var mr13 = localStorage.getItem('mrs13');
+        var mr14 = localStorage.getItem('mrs14');
+        var mr15 = localStorage.getItem('mrs15');
+        var mr16 = localStorage.getItem('mrs16');
+        var mr17 = localStorage.getItem('mrs17');
+        var mr18 = localStorage.getItem('mrs18');
+        var mr19 = localStorage.getItem('mrs19');
+        var mr20 = localStorage.getItem('mrs20');
+       
+        
+        
+        $('#8am').text(mr1);
+        $('#9am').text(mr2);
+        $('#10am').text(mr3);
+        $('#11am').text(mr4);
+        $('#12pm').text(mr5);
+        $('#1pm').text(mr6);
+        $('#2pm').text(mr7);
+        $('#3pm').text(mr8);
+        $('#8am').text(mr9);
+        $('#9am').text(mr10);
+        $('#10am').text(mr11);
+        $('#11am').text(mr12);
+        $('#12pm').text(mr13);
+        $('#1pm').text(mr14);
+        $('#2pm').text(mr15);
+        $('#3pm').text(mr16);
+        $('#12pm').text(mr17);
+        $('#1pm').text(mr18);
+        $('#2pm').text(mr19);
+        $('#3pm').text(mr20);
+    }
+ }
+function writeHourRows() {
+    for (let i = 0; i < hours.length; i++) {
+        var rowEl = $('<section>');
+        var hourDiv = $('<div>');
+        var textAreaSection = $('<textarea>');
+        var rowBtn = $('<div>');
+        var rowBtnImage = $('<img>');
+         // Use IF-Then statement to change row color
+        if (hoursInts[i] === currentTimeHr) {
+            hourRowStyle = "present";
+        } else if (Number(hoursInts[i]) > Number(curentTimeHrInt)) {
+            hourRowStyle = "future";
+        } else {
+            hourRowStyle = "past";
+        }
+        // Create Row Element
+        rootEl.append(rowEl);
+        rowEl.attr('class', 'row');
+        // Create Hour column
+        rowEl.append(hourDiv);
+        hourDiv.text(hours[i]);
+        hourDiv.attr('class', 'hour');
+        // Create Calendar Event Field
+        rowEl.append(textAreaSection);
+        textAreaSection.attr('class', hourRowStyle);
+        textAreaSection.attr('id', hours[i]);
+        // Create Save Button
+        rowEl.append(rowBtn);
+        rowBtn.attr('class', 'saveBtn');
+        rowBtn.attr('data-index', hours[i]);
+        // Append Image to Button DIV
+        rowBtn.append(rowBtnImage);
+        rowBtnImage.attr('src', './assets/images/save.png');
+    }
+}
+rootEl.click(function (event) {
+    var element = event.target;
+    if (element.matches("span") === true) {
+        var index = element.parentElement.getAttribute("data-index");
+        var meetingDes = $("#" + index).get(0).value;
+        localStorage.setItem(index, meetingDes);
+    }
+})
+timeEl.text(currentTime);
+writeHourRows();
+writeFromLocalStorage ()
+
 // var map;
 // var service;
 // var infowindow;
